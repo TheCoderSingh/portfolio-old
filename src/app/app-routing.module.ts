174fa51b6e30355dd260b5849c22f766529523c2
Jaskaran Singh <jaskaran1523@gmail.com>
2020-03-06 22:06:10 -0800

    Co-op commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,9 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './components/views/home/home.component';
-import { AboutComponent } from './components/views/about/about.component';
-import { ProjectsComponent } from './components/views/projects/projects.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'about', component: AboutComponent },
-  { path: 'projects', component: ProjectsComponent }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,13 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/commons/navbar/navbar.component';
 import { HomeComponent } from './components/views/home/home.component';
 import { FooterComponent } from './components/footer/footer.component';
-import { AboutComponent } from './components/views/about/about.component';
-import { ProjectsComponent } from './components/views/projects/projects.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     HomeComponent,
-    FooterComponent,
-    AboutComponent,
-    ProjectsComponent
+    FooterComponent
   ],
   imports: [
     BrowserModule,
